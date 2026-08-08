f4ae4a17f5453dd5cfde24e7619824ade285832e
yayza <y.aezah@gmail.com>
2026-08-07 17:11:02 -0700

    update

--- src/example.test.ts
+++ src/example.test.ts
@@ -1,36 +1,26 @@
-import 'reflect-metadata';
-import { Entity, PrimaryKey, Property, ReflectMetadataProvider } from '@mikro-orm/decorators/legacy';
-import { MikroORM } from '@mikro-orm/sqlite';
+import { defineEntity, MikroORM } from '@mikro-orm/postgresql';
 
-@Entity()
-class User {
-
-  @PrimaryKey()
-  id!: number;
-
-  @Property()
-  name: string;
-
-  @Property({ unique: true })
-  email: string;
-
-  constructor(name: string, email: string) {
-    this.name = name;
-    this.email = email;
-  }
-
-}
+const Product = defineEntity({
+  name: 'Product',
+  properties: p => ({
+    id: p.integer().primary(),
+    price: p.decimal().precision(12).scale(2).default('0.00'),
+  }),
+});
 
 let orm: MikroORM;
 
 beforeAll(async () => {
   orm = await MikroORM.init({
-    dbName: ':memory:',
-    entities: [User],
-    metadataProvider: ReflectMetadataProvider,
-    debug: ['query', 'query-params'],
-    allowGlobalContext: true, // only for testing
+    dbName: 'mikro_orm_repro',
+    host: process.env.DB_HOST ?? '127.0.0.1',
+    port: +(process.env.DB_PORT ?? 5432),
+    user: process.env.DB_USER ?? 'postgres',
+    password: process.env.DB_PASSWORD ?? 'postgres',
+    entities: [Product],
+    allowGlobalContext: true,
   });
+  await orm.schema.ensureDatabase();
   await orm.schema.refresh();
 });
 
@@ -38,17 +28,7 @@ afterAll(async () => {
   await orm.close(true);
 });
 
-test('basic CRUD example', async () => {
-  orm.em.create(User, { name: 'Foo', email: 'foo' });
-  await orm.em.flush();
-  orm.em.clear();
-
-  const user = await orm.em.findOneOrFail(User, { email: 'foo' });
-  expect(user.name).toBe('Foo');
-  user.name = 'Bar';
-  orm.em.remove(user);
-  await orm.em.flush();
-
-  const count = await orm.em.count(User, { email: 'foo' });
-  expect(count).toBe(0);
+test('schema is up to date immediately after being created', async () => {
+  const sql = await orm.schema.getUpdateSchemaSQL();
+  expect(sql).toBe('');
 });
